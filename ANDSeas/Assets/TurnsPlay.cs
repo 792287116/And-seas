a92cd4748e3a291d1e787e8dbe3238664cdf46e7
Huang Sheng Jun <792287116@qq.com>
2017-11-10 18:06:36 +0800

    提交unity项目

---
+++ ANDSeas/Assets/TurnsPlay.cs
@@ -0,0 +1,126 @@
+﻿/*轮播*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class TurnsPlay : MonoBehaviour {
+    public Button buttonBefore;
+    public Button buttonNext;
+    public Image[] picture;
+    public Sprite[] sprites;
+    Tweener turnTween;//位移动画
+    Sequence sequence;
+    float w;
+    int index = 1;//图片序号
+    bool isBox0;
+	// Use this for initialization
+	void Start () {
+        w = picture[0].GetComponent<RectTransform>().sizeDelta.x;
+        picture[0].sprite = sprites[0];
+        isBox0 = true;
+        picture[1].gameObject.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlayTween(bool left) {
+        //超出图集数量归零
+        if (index >= sprites.Length)
+        {
+            index = 0;
+        }
+        Debug.Log(index);
+        if (isBox0)
+        {
+            picture[1].sprite = sprites[index++];
+        }
+        else
+        {
+            picture[0].sprite = sprites[index++];
+        }
+        if (left){
+            //向左
+            if (isBox0)
+            {
+                PlayT(1,true);
+            }
+            else {
+                PlayT(1, false);
+            }
+        }
+        else {
+            //向右
+            if (isBox0)
+            {
+                PlayT(-1,true);
+                //picture[1].transform.localPosition = new Vector3(-w, 0, 0);
+                //picture[1].gameObject.SetActive(true);
+                //picture[1].transform.DOLocalMoveX(0, 0.3f)
+                //     .OnComplete(() => {
+                //         picture[1].gameObject.SetActive(true);
+                //     });
+                //picture[0].transform.DOLocalMoveX(w, 0.3f)
+                //    .OnComplete(() => {
+                //        picture[0].gameObject.SetActive(false);
+                //    });
+                //picture[1].transform.DOScale(Vector3.one, 0.3f);
+                //picture[0].transform.DOScale(Vector3.zero, 0.3f);
+            }
+            else
+            {
+                PlayT(-1,false);
+                //picture[0].transform.localPosition = new Vector3(-w, 0, 0);
+                //picture[0].gameObject.SetActive(true);
+                //picture[0].transform.DOLocalMoveX(0, 0.3f)
+                //     .OnComplete(() => {
+                //         picture[0].gameObject.SetActive(true);
+                //     });
+                //picture[1].transform.DOLocalMoveX(w, 0.3f)
+                //    .OnComplete(() => {
+                //        picture[1].gameObject.SetActive(false);
+                //    });
+                //picture[0].transform.DOScale(Vector3.one, 0.3f);
+                //picture[1].transform.DOScale(Vector3.zero, 0.3f);
+            }
+        }
+        
+        isBox0 = !isBox0;
+    }
+
+    private void PlayT(int left,bool box0) {
+        int i, j;
+        if (box0)
+        {
+            i = 1;
+            j = 0;
+        }
+        else {
+            i = 0;
+            j = 1;
+        }
+        picture[i].transform.localPosition = new Vector3(left*w, 0, 0);
+        picture[i].gameObject.SetActive(true);
+        picture[i].transform.DOScale(Vector3.one, 0.3f);
+        picture[i].transform.DOLocalMoveX(0, 0.3f)
+            .OnComplete(() => {
+                picture[i].gameObject.SetActive(true);
+            });
+        picture[j].transform.DOScale(Vector3.zero, 0.3f);
+        picture[j].transform.DOLocalMoveX(-w, 0.3f)
+            .OnComplete(() => {
+                picture[j].gameObject.SetActive(false);
+            });
+    }
+
+    public void PlayBefore() {
+        PlayTween(true);
+    }
+    public void PlayNext() {
+        PlayTween(false);
+    }
+}
---
+++ Rocker.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Rocker : MonoBehaviour {
+    private Vector3 parentV;
+    private float r;
+    public GameObject player;
+    private bool isWalk;
+    Vector3 dir;
+    // Use this for initialization
+    void Start () {
+        parentV = this.GetComponentInParent<RectTransform>().position;
+        //r = Vector3.Distance(this.transform.position,parentV);
+        r = this.GetComponentInParent<RectTransform>().sizeDelta.x;
+
+        this.gameObject.AddComponent<EventTrigger>();
+        EventTrigger joystick = this.gameObject.GetComponent<EventTrigger>();
+        EventTrigger.Entry joystickDrag = new EventTrigger.Entry();
+        joystickDrag.eventID = EventTriggerType.Drag;
+        joystickDrag.callback.AddListener((data)=> { OnpointerDrag((PointerEventData)data); });
+        joystick.triggers.Add(joystickDrag);
+        EventTrigger.Entry joystickDragEnd = new EventTrigger.Entry();
+        joystickDragEnd.eventID = EventTriggerType.EndDrag;
+        joystickDragEnd.callback.AddListener((data)=> { OnpointerDragEnd((PointerEventData)data); });
+        joystick.triggers.Add(joystickDragEnd);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (isWalk)
+        {
+            player.transform.position += 2*(this.transform.position - parentV) * Time.deltaTime;
+        }
+	}
+
+    private void OnpointerDrag(PointerEventData data) {
+        if (Vector3.Distance(data.position, parentV) < r)
+        {
+            this.transform.position = data.position;
+        }
+        else {
+            dir = Input.mousePosition - parentV;
+            transform.position = parentV + dir.normalized * r;
+        }
+        isWalk = true;
+    }
+
+    private void OnpointerDragEnd(PointerEventData data) {
+        this.transform.position = parentV;
+        isWalk = false;
+    }
+}
